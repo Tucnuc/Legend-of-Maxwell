a62c8636ac4de16e1b459bf7bcd1365395e3f6ae
Adam Černý <147615015+Tucnuc@users.noreply.github.com>
2024-04-27 21:21:09 +0200

    Update 8.0 - Text UI

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+// -----------
+//  MAIN GAME
+// -----------
+
+//  VARIABLES
+// -----------
+
+// MAIN VARIABLES
+let hardMode = false;
+let corruptionCore = false;
+
+// USER VARIABLES
+let name = "";
+let userRank = "Začátečník";
+let userHP = 100;
+let userMaxHP = 100;
+let userGold = 0;
+
+let userWeapon = "Dřevěný Meč";
+let userWeaponTier = 0;
+let userMinDmg = 2;
+let userMaxDmg = 5;
+
+let userArmor = "Žádné";
+let userArmorBonus = 0;
+let userArmorTier = 0;
+
+// SHOP VARIABLES
+let shopBan = false;
+
+// TEXT OBJECTS
+const spawnText = {
+    text1: 'Nacházíš se na spawnu. Rozhlédneš se kolem sebe.',
+    text2: 'Na levo vidíš obchod. Můžeš jít nakupovat k obchodníkovi Konstantinovi. Prodává zboží z celé říše!',
+    text3: 'Před sebou také vidíš magickou fontánu se zářící vodou. Zdejší léčící fontána z legend. Vyléčí vše.',
+    text4: 'Za fontánou zahlédneš obří bránu. Je to ta brána o které si četl ve všech těch knížkách.',
+    text5: 'Porta Magnifica! Magická brána, která tě může udělat silnějším. Drží v sobě nepředstavitelné bohatství. Ale také spoustu nebezpečí...'
+}
+
+const shopText = {
+    text1: 'Zdravím tě, zákazníku. Konstantin jméno mé. Lepší zboží nenajdeš široko daleko!',
+    text2: 'Mohu ti nabídnout zbraně a brnění všeho druhu. V nejlepší kvalitě samozřejmě a za nejlepší cenu!'
+}
+
+const weaponText = {
+    text1: 'Potřebujete nějakou zbraň? Drahý zákazníku, hned vám něco dobrého nabídnu...'
+}
+
+
+
+
+
+// -------------
+//  OTHER STUFF
+// -------------
+
 // BUTTONS SOUND EFFECTS
 let buttonSound = new Audio('./Music-Library/button.wav');
 const buttons = document.querySelectorAll('.button')
@@ -19,8 +75,6 @@ buttons.forEach(button => {
 // NAME MENU
 const nameBlur = document.getElementById('backgroundStartBlur');
 const nameMenu = document.querySelector('.containerName');
-
-let name = '';
 
 document.getElementById('playBtn').addEventListener('click', () => {
     name = document.getElementById('nameInput').value;
@@ -64,6 +118,17 @@ statusButton.addEventListener('click', () => {
 
                 // ASSIGNING VALUES
                 document.querySelector('.statusName').innerHTML = `Jméno: ${name}`;
+                document.querySelector('.statusRank').innerHTML = `Hodnost: ${userRank}`;
+                document.querySelector('.statusHP').innerHTML = `Body života: ${userHP}/${userMaxHP}`;
+                document.querySelector('.statusGold').innerHTML = `Zlato: ${userGold}`;
+
+                document.querySelector('.statusWeapon').innerHTML = `Zbraň: ${userWeapon}`;
+                document.querySelector('.statusDmg').innerHTML = `Útočná síla: ${userMinDmg}-${userMaxDmg}`;
+                document.querySelector('.statusWepTier').innerHTML = `Úroveň zbraně: ${userWeaponTier}`;
+
+                document.querySelector('.statusArmor').innerHTML = `Brnění: ${userArmor}`;
+                document.querySelector('.statusArmorBonus').innerHTML = `Bonusové životy: ${userArmorBonus}`;
+                document.querySelector('.statusArmTier').innerHTML = `Úroveň Brnění: ${userArmorTier}`;
 
                 heading.setAttribute('appear', "");
                 textCon.setAttribute('appear', "");
@@ -134,4 +199,33 @@ testBtn.addEventListener('click', () => {
     zoneChange(11);
 })
 
+// TEXT UI
+const textDiv = document.getElementById('textUI');
+let isWriting = false;
 
+function aniText(textArray) {
+    const keys = Object.keys(textArray);
+    let index = 0;
+
+    textDiv.addEventListener('click', () => {
+        if (isWriting === false) {
+            isWriting = true;
+            aniText2(textArray[keys[index]]);
+            index = (index + 1) % keys.length;
+        }
+    });
+};
+
+function aniText2(text, i = 0) {
+    if (i === 0) {
+        textDiv.textContent = '';
+    }
+    textDiv.textContent += text[i];
+    if (i < text.length - 1) {
+        setTimeout(() => aniText2(text, i + 1), 15);
+    } else {
+        isWriting = false;
+    }
+};
+
+aniText(spawnText);
